c21dc9f2c701061ef92396fe5a3e61a01dc75a7e
Rojo <u1802563@gmail.com>
2024-12-17 12:55:40 -0500

    Initial FastAPI Meme Website setup

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route('/')
-def home():
-    memes = [
-        {"src": "images/meme1.jpg", "alt": "Meme 1", "link": "https://example1.com"},
-        {"src": "images/meme2.jpg", "alt": "Meme 2", "link": "https://example2.com"},
-    ]
-    return render_template('index.html', memes=memes)
+# Configuración de carpetas estáticas y plantillas
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# Datos de ejemplo
+MEMES = [
+    {"src": "images/meme1.jpg", "alt": "Meme 1", "link": "https://example1.com"},
+    {"src": "images/meme2.jpg", "alt": "Meme 2", "link": "https://example2.com"},
+]
+
+SOCIAL_LINKS = {
+    "Twitter": "https://twitter.com/example",
+    "Instagram": "https://instagram.com/example",
+    "Facebook": "https://facebook.com/example"
+}
+
+# Ruta principal
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "memes": MEMES, "social_links": SOCIAL_LINKS})
